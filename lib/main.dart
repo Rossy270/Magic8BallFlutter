04816885e781e8baaad38d8eca28f73119babc3a
rossyKasteleria <rossy.kasteleria@gmail.com>
2023-12-20 14:35:26 -0300

    Projeto Completo

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blue[300],
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent.shade700,
+        title: const Text("Ask me Anything"),
+        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 30),
+        elevation: 3,
+        shadowColor: Colors.black,
+        centerTitle: true,
+      ),
+      body: const MagicBallPage(),
+    ),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MagicBallPage extends StatefulWidget {
+  const MagicBallPage({super.key});
+
+  @override
+  State<MagicBallPage> createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  int index = 1;
+
+  void getAnswer(){
+    setState(() {
+      var lastIndex = index;
+      while(lastIndex == index){
+        index = Random().nextInt(5) + 1;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Center(
+      child: Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: TextButton(
+            onPressed: (){
+              getAnswer();
+            },
+            child: Image.asset('imagens/ball$index.png'),
+          ),
         ),
       ),
     );
